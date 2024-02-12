c02683b9c946c46b98dc3185d4a291fb10a60a9f
LienLeinad <neil@bukas.ph>
2024-02-12 18:46:20 +0800

    add blank views

--- life_tracker/views.py
+++ life_tracker/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
 # Create your views here.
 
-class LifeTrackerView(View):
-    pass
+class LifeTrackerView(APIView):
+    def get(self, request, format=None):
+        pass
+
+    def post(self,request,format=None):
+        pass
