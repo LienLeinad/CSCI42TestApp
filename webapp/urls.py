bddefba428e8cedb2bd15f099e803971e2e4aadd
LienLeinad <neil@bukas.ph>
2024-02-12 18:38:18 +0800

    add drf

--- webapp/urls.py
+++ webapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from life_tracker.views import LifeTrackerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('life_tracker/', LifeTrackerView.as_view())
 ]
