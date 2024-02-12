dd381ba8641644d0422281ca3f405d4dd89b9f2c
LienLeinad <neil@bukas.ph>
2024-02-12 18:46:36 +0800

    fix lint

--- life_tracker/views.py
+++ life_tracker/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+
 # Create your views here.
+
 
 class LifeTrackerView(APIView):
     def get(self, request, format=None):
         pass
 
-    def post(self,request,format=None):
+    def post(self, request, format=None):
         pass
