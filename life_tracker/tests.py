88de71631d1b367c233fbd99e80f0c20696fdba5
LienLeinad <neil@bukas.ph>
2024-02-15 21:32:17 +0800

    add some more tests

--- life_tracker/tests.py
+++ life_tracker/tests.py
@@ -27,3 +27,28 @@ class LifeTrackerTestCase(APITestCase):
             LifeCounter.objects.exists(),
             "Life Counter object should be created upon calling the url when no life counter object exists",
         )
+        with self.subTest("Test for existing life counter"):
+            LifeCounter.objects.update(p1_life=20, p2_life=30)
+
+            response = self.client.get(self.url)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data.get("p1_life"), 20)
+            self.assertEqual(response.data.get("p2_life"), 30)
+
+            self.assertEqual(1, LifeCounter.objects.count())
+
+    def test_reset(self):
+        """
+        Expected Behavior:
+            When POST /life_counter/reset/ is called, Life counter is set back to 40 for both players
+        """
+        url = f"{self.url}/reset/"
+        response = self.client.post(self.url)
+
+    def test_patch(self):
+        """
+        Expected Request Body:
+            url: PATCH /life_counter/<player_number: p1 or p2>/increment/
+            or
+            url: PATCH /life_counter/<player_number>/decrement/
+        """
